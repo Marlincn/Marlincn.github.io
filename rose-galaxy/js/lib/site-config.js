f5b106b8e9474307e1090706f64768d30147268d
deymo <2650470260@qq.com>
2026-08-31 07:31:11 +0800

    Site updated: 2026-08-31 07:31:01

---
+++ rose-galaxy/js/lib/site-config.js
@@ -0,0 +1,1 @@
+(function(){"use strict";window.NOVA_SITE={bili:{proxy:"https://1470690781-6b1hcscil5.ap-guangzhou.tencentscf.com",uid:"3546712446601247",folder:"music"}}})();
---
+++ rose-galaxy/js/lib/utils.js
@@ -0,0 +1,1 @@
+(function(){"use strict";window.NOVA_UTILS={formatTime:function(r){if(!Number.isFinite(r)||r<0)return"00:00";var i=Math.floor(r/60),t=Math.floor(r%60).toString().padStart(2,"0");return i+":"+t}}})();
