6aed9bca9c16d54389d7180438cd7ef5be371cad
deymo <2650470260@qq.com>
2026-09-02 18:42:58 +0800

    Site updated: 2026-09-02 18:42:57

--- rose-galaxy/js/lib/utils.js
+++ rose-galaxy/js/lib/utils.js
@@ -1,1 +1,1 @@
-(function(){"use strict";window.NOVA_UTILS={formatTime:function(t){if(!Number.isFinite(t)||t<0)return"00:00";var r=Math.floor(t/60),i=Math.floor(t%60).toString().padStart(2,"0");return r+":"+i}}})();
+(function(){"use strict";window.NOVA_UTILS={formatTime:function(r){if(!Number.isFinite(r)||r<0)return"00:00";var i=Math.floor(r/60),t=Math.floor(r%60).toString().padStart(2,"0");return i+":"+t}}})();
